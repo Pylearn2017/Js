715bd06c003287539f4abe2ff453d13c53084857
Python 3 Strogino <33227955+Pylearn2017@users.noreply.github.com>
2019-12-16 18:09:06 +0300

    codingChallenge4

---
+++ JS/codingChallenge5/js/main.js
@@ -0,0 +1,18 @@
+// function tipCalculator (bill) {
+// 	bill <= 50 ? tip = bill * 0.20 : 
+// 	bill >= 50 && bill < 200 ? tip = bill * 0.15 :
+// 	bill >= 200 ? tip = bill * 0.10
+// 	return tip
+// }
+
+// firstTip = tipCalculator(124)
+// console.log(firstTip)
+
+
+// var billValues = {
+// 	bills:[124, 48, 268, 180 42],
+// 	calculateTip: function() {
+// 		console.log()
+// 	}
+// }
+console.log(12 * 3 * 50)
